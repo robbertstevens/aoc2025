3adc1c11b17bccb66956b2839d404041c44174b0
Robbert Stevens <robbertstevens@gmail.com>
2025-12-01 21:00:11 +0100

    feat: day 1 part 1

---
+++ src/day-1.js
@@ -0,0 +1,78 @@
+import {readFile} from "node:fs/promises";
+
+
+export class Wrapnum {
+    constructor(start, min, max) {
+        this.index = start
+        this.min = min
+        this.max = max
+    }
+
+    /**
+     *
+     * @param {number}num
+     * @returns {number}
+     */
+    increment(num) {
+        for (let i = 0;  i < Math.abs(num);i++  ) {
+            this.index += Math.sign(num)
+
+            if (this.index > this.max) {
+                this.index = this.min
+            }
+            if(this.index < this.min) {
+                this.index = this.max
+            }
+        }
+
+        return this.index
+    }
+}
+
+/**
+ * @param {string[]} input
+ */
+export function findCode(input) {
+    const wrapnum = new Wrapnum(50, 0, 99);
+
+    const history = []
+
+    const digits = input.map((i) => {
+        if (i.startsWith('L')) {
+            return Number(-i.slice(1, i.length))
+        }
+
+        if (i.startsWith("R")) {
+            return Number(i.slice(1, i.length))
+        }
+    })
+
+    for (const digit of digits) {
+        history.push(wrapnum.increment(digit))
+    }
+
+    return history
+}
+
+//
+const commands = [
+    "L68",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
+]
+
+const body = await readFile('./input/day-1.txt', "utf-8")
+
+// console.log(body.split('\n'))
+
+// console.log(findCode(commands).filter(i => i === 0).length)
+console.log(findCode(body.split('\n')).filter(i => i === 0).length)
+
+
---
+++ tests/wrap.test.js
@@ -0,0 +1,34 @@
+import {expect, test} from 'vitest'
+import {findCode, Wrapnum} from "../src/day-1";
+
+test.each([
+    [0, -1, 99],
+    [0, -2, 98],
+    [99, 1, 0],
+    [99, 2, 1],
+    [99, 0, 99],
+    [5, -10, 95],
+    [11, 8, 19],
+    [0, 420, 20]
+])("start %s increments by %s results in %s", (start, value, expected) => {
+    const wrapnum = new Wrapnum(start, 0, 99)
+
+    expect(wrapnum.increment(value)).toBe(expected)
+})
+
+test("sequence", () => {
+    const input = [
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82",
+    ]
+
+    expect(findCode(input).filter(i=> i===0).length).toBe(3)
+})
