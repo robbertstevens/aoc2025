b21dfccff2547c8a2199c9d952daa5c219d4fbf6
Robbert Stevens <robbertstevens@gmail.com>
2025-12-05 22:59:05 +0100

    day 5 part 2

--- src/day-5.js
+++ src/day-5.js
@@ -7,7 +7,7 @@ import {readFile} from "node:fs/promises";
  */
 function checkRanges(ranges, id) {
     for (const [start, end] of ranges) {
-        if (id > start && id < end) {
+        if (id > start && id <= end) {
             return true
         }
     }
@@ -21,7 +21,7 @@ export function process() {
     /**
      * @param {string} input
      */
-    const fn =  (input) => {
+    const fn = (input) => {
         const lines = input.split('\n')
         const freshIngredients = []
 
@@ -50,8 +50,66 @@ export function process() {
     return fn
 }
 
+
+export function freshAccordingToRanges() {
+    let count = BigInt(0)
+    const ranges = []
+
+    const fn = (input) => {
+        const lines = input.split('\n')
+
+        for (const line of lines) {
+            if (!line.includes("-")) {
+                continue
+            }
+            const [start, end] = line.split('-')
+
+            ranges.push([BigInt(start), BigInt(end)])
+        }
+
+        const RANGE_START = 0
+        const RANGE_END = 1
+
+        const max = (a, b) => a > b ? a : b
+
+
+        const merge = (intervals) => {
+            intervals.sort((a, b) => {
+                return a[RANGE_START] > b[RANGE_START] ? 1 : -1;
+            })
+
+            const merged = [intervals[0]]
+
+            for (const range of intervals) {
+                const lm = merged[merged.length - 1]
+
+                if (range[RANGE_START] <= lm[RANGE_END]) {
+                    lm[RANGE_END] = max(range[RANGE_END], lm[RANGE_END])
+                } else {
+                    merged.push(range)
+                }
+            }
+
+            return merged
+        }
+
+        const mergedRanges = merge(ranges)
+
+        for (const [start, end] of mergedRanges) {
+            count += (end - start) + 1n
+        }
+
+        return count
+
+    }
+
+    return fn
+}
+
 const data = await readFile('./input/day-5.txt', 'utf-8')
 
 const checker = process()
+const checker2 = freshAccordingToRanges()
 
 console.log(checker(data).length)
+console.log(checker2(data))
--- tests/ingredient-checker.test.js
+++ tests/ingredient-checker.test.js
@@ -1,8 +1,7 @@
 import {expect, test} from 'vitest'
-import {process} from "../src/day-5";
+import {freshAccordingToRanges, process} from "../src/day-5";
 
-test('fresh ingredients checker', () => {
-    const input = `3-5
+const input = `3-5
 10-14
 16-20
 12-18
@@ -13,7 +12,15 @@ test('fresh ingredients checker', () => {
 11
 17
 32`
+
+test('fresh ingredients checker', () => {
     const checker = process()
 
     expect(checker(input).length).toBe(3)
+})
+
+test('calculate all ranges', () => {
+   const checker = freshAccordingToRanges()
+
+    expect(checker(input)).toBe(14n)
 })
